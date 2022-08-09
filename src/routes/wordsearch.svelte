2d9a909a0747e37a053fce64d82b8c6d28dfaca8
Nv7-GitHub <50369270+Nv7-GitHub@users.noreply.github.com>
2022-08-08 22:02:29 -0700

    Implement search algorithm

--- src/routes/wordsearch.svelte
+++ src/routes/wordsearch.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-  let inp = `xymbitackkj
+  const orig = `xymbitackkj
 kxfyofasokd
 sxtmttcbmwh
 rranatbspkh
@@ -9,6 +9,7 @@ rkemcsgjndn
 ssecqacizlw
 eahcfxrbkyk
 cbslmqxtrla`;
+  let inp = orig;
 
   let board: string[][] = [];
 
@@ -33,8 +34,48 @@ cbslmqxtrla`;
 
   $: {
     reset_matched();
-    let q = query.toLowerCase();
-    
+    let q = Array.from(query.toLowerCase());
+    for (let [r, row] of board.entries()) { // Loop over rows & cols
+      for (let [c, col] of row.entries()) {
+        if (board[r][c] == q[0]) { // Character matches, see if areas nearby match
+          for (let roff = -1; roff <= 1; roff++) { // Loop through offsets
+            for (let coff = -1; coff <= 1; coff++) {
+              if (roff == 0 && coff == 0) { // Not an offset
+                continue;
+              }
+
+              // Loop through letters & match
+              let valid = true;
+              for (let [i, lett] of q.entries()) {
+                if (i == 0) {
+                  continue; // We have already matched first letter
+                }
+                
+                // Calc indexes
+                let rv = r + (roff * i);
+                let cv = c + (coff * i);
+
+                // Checks
+                if (rv < 0 || rv >= board.length || cv < 0 || cv >= board[rv].length) {
+                  valid = false;
+                  break; // Continue to next offset if out of bounds
+                }
+                if (board[rv][cv] != lett) {
+                  valid = false;
+                  break; // Continue if doesn't match
+                }
+              }
+
+              if (valid) { // Found word, add to matched
+                for (let i = 0; i < q.length; i++) {
+                  matched[r + (roff * i)][c + (coff * i)] = true;
+                }
+              }
+            }
+          }
+        }
+      }
+    }
   }
 </script>
 
@@ -45,6 +86,22 @@ cbslmqxtrla`;
 <textarea class="form-control" bind:value={inp} rows={inp.length - (inp.replaceAll("\n", "").length) + 1}></textarea>
 <button class="btn btn-primary mt-2" on:click={load}>Load Board</button>
 {:else}
+
+{#if inp == orig}
+<div class="text-start">
+  <p>This is the default puzzle! Some words to look for are:</p>
+  <ul>
+    <li>cow</li>
+    <li>tomato</li>
+    <li>tractor</li>
+    <li>horse</li>
+    <li>sheep</li>
+    <li>basket</li>
+    <li>cat</li>
+  </ul>
+</div>
+{/if}
+
 <input type="text" class="form-control" bind:value={query} placeholder="Word to find..."/>
 
 <div class="table-responsive">
