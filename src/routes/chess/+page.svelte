079baf8bdc5dcf6ac4151fe96fdfa73012029388
Nv7 <50369270+Nv7-GitHub@users.noreply.github.com>
2022-10-05 05:12:32 +0000

    Inject chess html

--- src/routes/chess/+page.svelte
+++ src/routes/chess/+page.svelte
@@ -1,2 +1,13 @@
+<script lang="ts">
+  import chesshtml from "$lib/assets/chess.html?raw";
+    import { onMount } from "svelte";
+  let frame: HTMLIFrameElement;
+  onMount(() => {
+    frame.contentWindow?.document.open();
+    frame.contentWindow?.document.write(chesshtml);
+    frame.contentWindow?.document.close();
+  });
+</script>
+
 <h1>Chess</h1>
-<iframe src="/chessframe/chess.html" width="480" height="540" title="Chess"></iframe>
+<iframe bind:this={frame} width="480" height="540" title="Chess"></iframe>
