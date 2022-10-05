5f47ee42cd9fded783d07f6d85cd15c736aae7a0
Nv7 <50369270+Nv7-GitHub@users.noreply.github.com>
2022-10-05 05:00:14 +0000

    Chessframe

--- src/routes/chess/+page.svelte
+++ src/routes/chess/+page.svelte
@@ -1,2 +1,2 @@
 <h1>Chess</h1>
-<iframe src="/chess/chess.html" width="480" height="540" title="Chess"></iframe>
+<iframe src="/chessframe/chess.html" width="480" height="540" title="Chess"></iframe>
